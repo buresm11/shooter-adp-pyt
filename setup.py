754ca3ad80086f6b989d2c5b7c19eb108dc51c61
Michal Bures <buresm11@fit.cvut.cz>
2018-01-17 07:38:29 -0800

    created setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	name='shooter',
+	version='0.1',
+	description='Shooting game',
+	author='Michal Bures',
+	author_email="buresm11@fit.cvut.cz",
+	keywords='game, shooting',
+	license='Public domain',
+	packages=['shooter'],
+	classifiers=[
+		'Intended Audience :: Gamers',
+    	'License :: Public Domain',
+    	'Programming Language :: Python'],
+    zip_safe=False
+)
